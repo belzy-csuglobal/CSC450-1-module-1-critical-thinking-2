8ac4583680a6a4aeece2214360adb159381ff705
Brandon Elzy <brandon.g.elzy@gmail.com>
2024-10-18 06:53:45 -0500

    Completed Assignment

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string firstName = "John";
+	string lastName = "Smith";
+	string street = "42 Maple Street";
+	string city = "Willowbrook City";
+	string zipCode = "12345";
+
+	cout << "First Name: " + firstName << endl;
+	cout << "Last Name: " + lastName << endl;
+	cout << "Street: " + street << endl;
+	cout << "City: " + city << endl;
+	cout << "Zip Code: " + zipCode << endl;
+
+	return 0;
+}
